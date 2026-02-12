f2a753e18b0e7782c67d4181fefaeef00a7db6d3
jamminjason1 <jsn.fontes@gmail.com>
2026-02-12 14:13:05 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+const CONFIG = {
+    mainQuestion: "Will you be my Valentine? 💌",
+    yesButtonText: "Yes",
+    noButtonText: "No",
+
+    popupMessages: [
+        "Are you sure?",
+        "More excited than that",
+        "One more time",
+        "Okay last time just to make sure"
+    ],
+
+    event: {
+        title: "💖 It's a Date! 💖",
+        date: "February 15, 2026",
+        time: "7:00 PM",
+        locationName: "Petrini's Italian",
+        locationCity: "San Diego, CA",
+        footerText: "See you soon, Hottie"
+    },
+
+    confettiEmojis: ["💖","✨","🎉","💘","🎊","💫"]
+};
